d6fdce83fe003a9de74ff15131f64ee37f09e94c
thecats1105 <thecats1105@gmail.com>
2024-08-09 16:51:24 +0900

    Edit REMOTE_DISTRO_URL system

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -1,8 +1,9 @@
 const { DistributionAPI } = require('helios-core/common')
+const version = '1.0.4-pre.1'
 
 const ConfigManager = require('./configmanager')
 
-exports.REMOTE_DISTRO_URL = 'https://data.japsik.xyz/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = `https://raw.githubusercontent.com/Japsik-Server/JapsikLauncher-Data/main/distribution/${version}.json`
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
